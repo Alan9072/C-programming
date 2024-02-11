bac559fbfce5a0f281a38b8f3b6a5c809befd826
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-02-11 09:37:02 +0530

    Update Mergesort.c

--- Mergesort.c
+++ Mergesort.c
@@ -3,7 +3,7 @@
 void merge(int arr[], int l, int m, int r) 
 { 
 	int i, j, k; 
-	int n1 = m - l + 1; 
+	int n1 = m - l + 1;   //can also be ( n1 = m+1 )
 	int n2 = r - m; 
 	int L[n1], R[n2]; 
 	for (i = 0; i < n1; i++) 
